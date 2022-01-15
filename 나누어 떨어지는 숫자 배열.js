6fa59b7690cc06520ef1e30e473305637ae58b8c
김정호 <43710866+fancyers@users.noreply.github.com>
2022-01-15 13:57:58 +0900

    Update 나누어 떨어지는 숫자 배열.js

--- 나누어 떨어지는 숫자 배열.js
+++ 나누어 떨어지는 숫자 배열.js
@@ -1,4 +1,5 @@
 function solution(arr, divisor) {
+    // arr 중에서 divisor로 나누어 떨어지는 값들을 result에 저장
     let result = []
     for (let i = 0; i < arr.length; i++) {
       let a = arr[i]
@@ -6,6 +7,8 @@ function solution(arr, divisor) {
         result.push(a)
       }
     }
+    //  result에 저장된 것이 없으면 [-1]을 return
     if (result.length == 0) {return [-1]}
+    // 오름차순 sort
     return result.sort(function(a,b){return a-b})
 }
