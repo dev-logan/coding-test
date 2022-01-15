1091f099ed44aa9e296f6a2d6a067d64bde249b2
김정호 <43710866+fancyers@users.noreply.github.com>
2022-01-15 13:59:15 +0900

    Update 서울에서 김서방 찾기.js

--- 서울에서 김서방 찾기.js
+++ 서울에서 김서방 찾기.js
@@ -1,4 +1,6 @@
 function solution(seoul) {
+    //seoul array 내에서 값이 'Kim'인 element의 첫번째 index 값 반환
     let idx = seoul.findIndex(name => name == 'Kim')
+    // 올바른 형식으로 return
     return '김서방은 '+idx+'에 있다'
 }
