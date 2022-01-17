5c27a75e43fe1df0ae5961248d9a460b41a9eaa1
김정호 <43710866+fancyers@users.noreply.github.com>
2022-01-17 11:37:02 +0900

    Update 제일 작은 수 제거하기.js

--- 제일 작은 수 제거하기.js
+++ 제일 작은 수 제거하기.js
@@ -1,7 +1,7 @@
 function solution(arr) {
-    if (arr.length == 1) return [-1]
+    if (arr.length == 1) return [-1]        //  원소가 하나인 array가 주어질 때
 
-    let min = Math.min.apply(null, arr)
-    let idx = arr.indexOf(min)
-    arr.splice(idx, 1)
+    let min = Math.min.apply(null, arr)     //  최소값 알기
+    let idx = arr.indexOf(min)              //  최소값의 좌표 알기
+    arr.splice(idx, 1)                      //  최소값 삭제
     return arr
