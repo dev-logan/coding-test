502d3ceadee7089402dae922ebcd14cb1edca199
김정호 <43710866+fancyers@users.noreply.github.com>
2022-01-17 11:33:27 +0900

    Update 자릿수 더하기.js

--- 자릿수 더하기.js
+++ 자릿수 더하기.js
@@ -1,10 +1,9 @@
 function solution(n)
 {
-    let sn = String(n)
-    let tot = 0
+    let sn = String(n)  //  숫자를 문자로 바꿈
+    let tot = 0         //  각 글자를 숫자로 다시 바꾼 뒤 더해줌
     for (let i in sn) {
       tot += parseInt(sn[i])
     }
-
     return tot
 }
