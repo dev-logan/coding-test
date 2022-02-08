1860d26971360b17d9fd339a2f09b52b629794e5
김정호 <43710866+fancyers@users.noreply.github.com>
2022-02-08 10:56:47 +0900

    Create 신나는 함수 실행.js

---
+++ 신나는 함수 실행.js
@@ -0,0 +1,57 @@
+// https://www.acmicpc.net/problem/9184
+
+
+const fs = require('fs')
+const input = fs.readFileSync('dev/stdin').toString().trim().split('\n')
+
+const memory = []
+
+const w = (a, b, c) => {
+    const [findMemory] = memory.filter((x) => {
+        return x[0] === a && x[1] === b && x[2] === c
+    })
+
+    if (findMemory) {
+        return findMemory[3]
+    } else {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            memory.push([a, b, c, 1])
+            return 1
+        } else if (a > 20 || b > 20 || c > 20) {
+            memory.push([a, b, c, w(20, 20, 20)])
+            return w(20, 20, 20)
+        } else if (a < b && b < c) {
+            memory.push([
+                a,
+                b,
+                c,
+                w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c),
+            ])
+            return w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
+        } else {
+            memory.push([
+                a,
+                b,
+                c,
+                w(a - 1, b, c) +
+                    w(a - 1, b - 1, c) +
+                    w(a - 1, b, c - 1) -
+                    w(a - 1, b - 1, c - 1),
+            ])
+            return (
+                w(a - 1, b, c) +
+                w(a - 1, b - 1, c) +
+                w(a - 1, b, c - 1) -
+                w(a - 1, b - 1, c - 1)
+            )
+        }
+    }
+}
+
+input.pop()
+const testCases = input.map((x) => x.split(' ')).map((x) => x.map((x) => +x))
+
+for (const testCase of testCases) {
+    const [a, b, c] = testCase
+    console.log(`w(${a}, ${b}, ${c}) = ${w(a, b, c)}`)
+}
